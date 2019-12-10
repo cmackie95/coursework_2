c3a90debb59cb409ba35e8984e3eab6078779df3
cmackie95 <connormackie@yahoo.co.uk>
2019-12-10 14:48:34 +0000

    Updated vm_configure.yml file and removed redundant files

--- kubernetes.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-# This script must be run using the sudo command
-# It will complete the Minikube installation and setup
-
-sudo kubectl apply -f /home/cmackie95/kubectl_pod_deployment.yml
